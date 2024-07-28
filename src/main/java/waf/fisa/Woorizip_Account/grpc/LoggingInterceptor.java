42edcc359e196738dac3051b99b45562b594a43d
Seongjun Kwon <tttzkq9336@gmail.com>
2024-07-28 21:38:35 +0900

    UPDATE set @Slf4j for log.

--- src/main/java/waf/fisa/Woorizip_Account/grpc/LoggingInterceptor.java
+++ src/main/java/waf/fisa/Woorizip_Account/grpc/LoggingInterceptor.java
@@ -1,18 +1,20 @@
 package waf.fisa.Woorizip_Account.grpc;
 
 import io.grpc.*;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class LoggingInterceptor implements ServerInterceptor {
 
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
             ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
-        System.out.println("** Call method: " + call.getMethodDescriptor().getFullMethodName());
-        System.out.println("** Headers: " + headers);
+        log.info("** Call method: {}", call.getMethodDescriptor().getFullMethodName());
+        log.info("** Headers: {}", headers);
         return next.startCall(new ForwardingServerCall.SimpleForwardingServerCall<ReqT, RespT>(call) {
             @Override
             public void sendMessage(RespT message) {
-                System.out.println("** Response: " + message);
+                log.info("** Response: {}", message);
                 super.sendMessage(message);
             }
         }, headers);
